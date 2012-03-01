4239fdafb4fc8d27a3bdeb4bcb9dc62fb71938bc
vikas yaligar <vikasyaligar.it@gmail.com>
2012-03-01 17:48:00 +0530

    Created Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Project1Check::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
