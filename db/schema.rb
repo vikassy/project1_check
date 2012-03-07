97943a160024226a47b53d056e5cd4a45f7611ae
vikas yaligar <vikasyaligar.it@gmail.com>
2012-03-07 15:46:42 +0530

    messed up some

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120305182908) do
+ActiveRecord::Schema.define(:version => 20120307073342) do
+
+  create_table "blogs", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "blogs", ["user_id", "created_at"], :name => "index_blogs_on_user_id_and_created_at"
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
